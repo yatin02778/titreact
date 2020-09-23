521639f7c47d589dec244f4cc48229292f6a5618
yatin02778 <yatin02778@gmail.com>
2020-09-23 15:35:49 +0530

    Updates

---
+++ precache-manifest.6750f646358d80cb284f029826940f25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827ab9a52693d0d74d04c3f34583a9e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e782608e4e0ebed3a4b6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d9d0ee62dafeface203b",
+    "url": "/static/js/2.b44cd623.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b44cd623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e782608e4e0ebed3a4b6",
+    "url": "/static/js/main.70f01439.chunk.js"
+  },
+  {
+    "revision": "ffe27a4bcd3de17ec39e",
+    "url": "/static/js/runtime-main.7c979398.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
